65ce6ff874f7d484eb43b3ce832b95bed2d1abc6
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-15 04:37:47 -0300

    Organizando itens do Painel principal. Criando Construtor para Título da Janela Principal, e setando algumas variáveis

--- src/view/Interface.java
+++ src/view/Interface.java
@@ -19,6 +19,7 @@ import model.*;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JDesktopPane;
+import java.awt.FlowLayout;
 
 public class Interface extends JFrame {
 	//Criando atributos da Interface Gráfica
@@ -30,6 +31,7 @@ public class Interface extends JFrame {
     private JButton btnConsultas;
     private JTextField campoRegistro;
     private JTextField campoEmail;
+    private JTextField campoTelefone;
     private JTextField campoEspecialidade;
     private JTextField campoNome;
     private JTextField campoPlanoSaude;
@@ -58,10 +60,102 @@ public class Interface extends JFrame {
     private JLabel labelQtdUsuarios;
     private JRadioButton radioMedico;
     private JRadioButton radioPaciente;
+    private JTextField txtTexto;
 	
-    
-    public void setLabels(){
-    	jLabel1.setText("Nome:");
+	public static void main(String[] args) {
+		
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface frame = new Interface("Programa Mais Médicos - Cadastro de Usuários - Versão 1.2");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	public Interface(String umaString){
+/******************************************************************************/
+/************************CRIANDO CONTAINER PRINCIPAL***************************/
+/******************************************************************************/
+		 super(umaString);
+		 JPanel contentPane = new JPanel();
+   	 	 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 setBounds(112, 90, 1142, 600);
+		 contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		 setContentPane(contentPane);
+		 contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		 
+		 JPanel panel = new JPanel();
+		 contentPane.add(panel);
+		 
+ /******************************************************************************/
+ /*****************************Criando OBJETOS************************************/
+ /******************************************************************************/
+		    btnBuscar = new JButton();
+	        btnCadastrar = new JButton();
+	        btnRemover = new JButton();
+	        btnConsultas = new JButton();
+	        
+	         campoRegistro = new JTextField();
+	         campoEmail = new JTextField();
+	         campoTelefone = new JTextField();
+	         campoEspecialidade = new JTextField();
+	         campoNome = new JTextField();
+	         campoPlanoSaude = new JTextField();
+	         campoRg = new JTextField();
+	         campoIdade = new JTextField();
+	        
+	         campoSintomas = new JTextArea();
+	        
+	         jLabel1 = new JLabel();
+	         jLabel2 = new JLabel();
+	         jLabel3 = new JLabel();
+	         jLabel4 = new JLabel();
+	         jLabel5 = new JLabel();
+	         jLabel6 = new JLabel();
+	         jLabel7 = new JLabel();
+	         jLabel8 = new JLabel();
+	         jLabel9 = new JLabel();
+	         jLabel10 = new JLabel();
+	         jLabel11 = new JLabel();
+	         jLabel12 = new JLabel();
+	         jLabel13 = new JLabel();
+	         jLabel14 = new JLabel();
+	         jLabel15 = new JLabel();
+	         jLabel16 = new JLabel();
+	         jLabel17 = new JLabel();
+	         labelQtdUsuarios = new JLabel();
+	        //Scroll
+	        jScrollPane1 = new JScrollPane();
+	       
+	        //checkbox
+	        radioMedico = new JRadioButton();
+	        radioPaciente = new JRadioButton();
+	        
+/******************************************************************************/
+/*********************************BOAS VINDAS AO USUARIO DO PROGRAMA*********************/
+/******************************************************************************/
+	        
+		 JOptionPane.showMessageDialog(null, "Olá, seja bem vindo ao Programa Mais Médicos.\n Este Programa foi desenvolvido para\n " +
+		 		"facilitar o Cadastramento de Consultas, Pacientes e Novos Médicos");
+		 
+ /****************************************************************************************/
+ /*********************************ORGANIZANDO PAINEL SUPERIOR****************************/
+ /****************************************************************************************/
+		campoNome.setColumns(20);
+		campoEmail.setColumns(20);
+		campoTelefone.setColumns(9);
+		campoRegistro.setColumns(12);
+		campoEspecialidade.setColumns(20);
+		campoIdade.setColumns(2);
+		campoPlanoSaude.setColumns(10);
+		campoRg.setColumns(8);
+		campoSintomas.setColumns(100);
+		 
+		jLabel1.setText("Nome:");
     	
         jLabel2.setText("E-mail:");
         
@@ -89,90 +183,24 @@ public class Interface extends JFrame {
 
         jLabel14.setText("Remédios/Medicações:");
 
-        
+        radioMedico.setText("Médico");
+        radioPaciente.setText("Paciente");
         
         btnCadastrar.setText("CADASTRAR");
         btnBuscar.setText("BUSCAR");
         btnRemover.setText("REMOVER");
         btnConsultas.setText("IR PARA CONSULTAS");
         
-    }
-	public static void main(String[] args) {
+        panel.add(jLabel1);
+        panel.add(campoNome);
+		panel.add(jLabel2);
+		panel.add(campoEmail);
+		panel.add(jLabel3);
+		panel.add(campoTelefone);
 		
 		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Interface frame = new Interface();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	public Interface(){
-/******************************************************************************/
-/************************CRIANDO CONTAINER PRINCIPAL***************************/
-/******************************************************************************/
-		 JPanel contentPane = new JPanel();
-   	 	 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 setBounds(112, 90, 1142, 600);
-		 contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		 contentPane.setLayout(new BorderLayout(0, 0));
-		 setContentPane(contentPane);
 		 
- /******************************************************************************/
- /*****************************Criando OBJETOS************************************/
- /******************************************************************************/
-		    JButton btnBuscar = new JButton();
-	        JButton btnCadastrar = new JButton();
-	        JButton btnRemover = new JButton();
-	        JButton btnConsultas = new JButton();
-	        
-	        JTextField campoRegistro = new JTextField();
-	        JTextField campoEmail = new JTextField();
-	        JTextField campoEspecialidade = new JTextField();
-	        JTextField campoNome = new JTextField();
-	        JTextField campoPlanoSaude = new JTextField();
-	        JTextField campoRg = new JTextField();
-	        JTextField campoIdade = new JTextField();
-	        
-	        JTextArea campoSintomas = new JTextArea();
-	        
-	        JLabel jLabel1 = new JLabel();
-	        JLabel jLabel2 = new JLabel();
-	        JLabel jLabel3 = new JLabel();
-	        JLabel jLabel4 = new JLabel();
-	        JLabel jLabel5 = new JLabel();
-	        JLabel jLabel6 = new JLabel();
-	        JLabel jLabel7 = new JLabel();
-	        JLabel jLabel8 = new JLabel();
-	        JLabel jLabel9 = new JLabel();
-	        JLabel jLabel10 = new JLabel();
-	        JLabel jLabel11 = new JLabel();
-	        JLabel jLabel12 = new JLabel();
-	        JLabel jLabel13 = new JLabel();
-	        JLabel jLabel14 = new JLabel();
-	        JLabel jLabel15 = new JLabel();
-	        JLabel jLabel16 = new JLabel();
-	        JLabel jLabel17 = new JLabel();
-	        JLabel labelQtdUsuarios = new JLabel();
-	        //Scroll
-	        JScrollPane jScrollPane1 = new JScrollPane();
-	       
-	        //checkbox
-	        JRadioButton radioMedico = new JRadioButton();
-	        JRadioButton radioPaciente = new JRadioButton();
-	        
-/******************************************************************************/
-/*********************************BOAS VINDAS AO PROGRAMA*********************/
-/******************************************************************************/
-	        
-		 JOptionPane.showMessageDialog(null, "Olá, seja bem vindo ao Programa Mais Médicos.\n Este Programa foi desenvolvido para\n " +
-		 		"facilitar o Cadastramento de Consultas, Pacientes e Novos Médicos");
-		 
-		 
+ 
    }
 
 }
